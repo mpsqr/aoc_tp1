63338eb1a3a72e6e7501aa2e31910cad690e54b9
Matéo Pasquier <pasquiermateo33@gmail.com>
2025-02-20 11:38:22 +0100

    loop invariant

--- kernel.c
+++ kernel.c
@@ -5,9 +5,10 @@
 /* original */
 void kernel (unsigned n , double *a, const float b[n] , const float c[n]) {
    unsigned i , j;
-   for ( j =0; j < n ; j ++) {
-      for ( i =0; i < n ; i ++) {
-         a[i*n + j] = log(pow(b[i] , c[j]));
+   for ( i =0; i < n ; i ++) {
+      int ind = i * n;
+      for ( j =0; j < n ; j ++) {
+         a[ind + j] = log(pow(b[i] , c[j]));
       }
    }
 }
